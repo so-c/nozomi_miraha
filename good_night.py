2cbe3179a569b8f18e7570df4ce5949a9ebd940e
so_c <so_c321@hotmail.co.jp>
2012-11-24 09:18:01 +0900

    randomize message.

--- good_night.py
+++ good_night.py
@@ -1,5 +1,16 @@
 #coding: utf-8
+import random
 from models import Account
 
+random.seed()
+r = random.randint(0, 2)
+
+if r == 0:
+    msg = u'眠たくなってきました……。おやすみなさい。'
+elif r == 1:
+    msg = u'おやすみなさい。あまり夜更かしすると体に毒ですよ？'
+elif r == 2:
+    msg = u'明日に備えて、そろそろ寝ることにします。おやすみなさい。'
+
 account = Account()
-account.tweet(u'そろそろ寝ることにします。おやすみなさい。')
+account.tweet(msg)
--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,16 @@
 #coding: utf-8
+import random
 from models import Account
 
+random.seed()
+r = random.randint(0, 2)
+
+if r == 0:
+    msg = u'おはようございます。今日も1日がんばりましょう。'
+elif r == 1:
+    msg = u'おはようございます。今日は何しようかな？'
+elif r == 2:
+    msg = u'おはようございまーす♪'
+
 account = Account()
-account.tweet(u'おはようございます。')
+account.tweet(msg)
