eb0f4d3cd56b3caf49e14013098ec9d4d60d8524
so_c <so_c321@hotmail.co.jp>
2012-11-25 14:45:04 +0900

    notify is a error occured when tweeting by email.

--- models.py
+++ models.py
@@ -1,14 +1,17 @@
+#coding: utf-8
 import ConfigParser
 import tweepy
+from tweepy.error import TweepError
+from google.appengine.api import mail
+
+CONFIG_FILE = 'config.ini'
 
 # Twitter Account
 class Account:
-    # login info
-    CONFIG_FILE = 'config.ini'
 
     def __init__(self):
         config = ConfigParser.SafeConfigParser()
-        config.read(self.CONFIG_FILE)
+        config.read(CONFIG_FILE)
 
         consumer_key        = config.get('consumer', 'key')
         consumer_secret     = config.get('consumer', 'secret')
@@ -19,5 +22,30 @@ class Account:
         auth.set_access_token(access_token, access_token_secret)
         self.api = tweepy.API(auth_handler=auth)
 
+    # tweet a messge.
     def tweet(self, message):
-        self.api.update_status(message)
+        try:
+            self.api.update_status(message)
+        except TweepError as e:
+            message = ErrorNotifier()
+            error_message = '{0} when tweet "{1}"'.format(e.reason, message)
+            message.notify_error()
+            raise
+
+# Error notifier by email
+class ErrorNotifier:
+
+    def __init__(self):
+        config = ConfigParser.SafeConfigParser()
+        config.read(CONFIG_FILE)
+
+        self.message = mail.EmailMessage()
+        self.message.subject = config.get('mail', 'subject')
+        self.message.sender  = config.get('mail', 'from')
+        self.message.to      = config.get('mail', 'to')
+
+    # send email.
+    def notify_error(self, body):
+        self.message.body = body
+        self.message.check_initialized()
+        self.message.send()
