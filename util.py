5ea871ea05e4fa9dd057f525d9374096b029f909
so_c <so_c321@hotmail.co.jp>
2012-12-01 03:20:23 +0900

    Unnested if closures.

--- util.py
+++ util.py
@@ -11,16 +11,14 @@ class MirahaParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         if tag == 'div':
             attrs = dict(attrs)
-            if 'class' in attrs:
-                if attrs['class'].find('nozomi') > -1:
-                    self.__is_nozomi = True
+            if 'class' in attrs and attrs['class'].find('nozomi') > -1:
+                self.__is_nozomi = True
 
     def handle_endtag(self, tag):
-        if self.__is_nozomi:
-            if tag == 'div':
-                self.__is_nozomi = False
-                self.dialogs.append(self.__current_str)
-                self.__current_str = ''
+        if self.__is_nozomi and tag == 'div':
+            self.__is_nozomi = False
+            self.dialogs.append(self.__current_str)
+            self.__current_str = ''
 
     def handle_data(self, data):
         if self.__is_nozomi:
