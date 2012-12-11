a664739141332dee3f637048aa21d128cbbe93c3
so_c <so_c321@hotmail.co.jp>
2012-12-11 23:51:47 +0900

    avoid to tweet duplicates.

--- quote.py
+++ quote.py
@@ -4,16 +4,35 @@ import random
 from models import Account
 from models import Blog
 from models import Post
+import logging
 
 class Quote(webapp2.RequestHandler):
     def get(self):
         blog = Blog('http://mirahalibrary.blogspot.com/feeds/posts/default')
         posts = blog.select_posts_bytag(u'鏡館')
 
+        msg = self.sample_msg(posts)
+        account = Account()
+        last_status = account.api.user_timeline(count=1)[0].text
+
+        logging.info('last status: ' + last_status)
+        logging.info('msg: ' + msg)
+
+        i = 0
+        while msg == last_status and i < 10:
+            msg = self.sample_msg(posts)
+            logging.info('msg: ' + msg)
+            i += 1
+
+        if i == 10:
+            msg = u'前回と違うつぶやきが見つかりません……。'
+
+        account.tweet(msg)
+
+    def sample_msg(self, posts):
         len_posts = len(posts)
         if len_posts <= 0:
-            # TODO 引用するつぶやきがない場合
-            pass
+            return u'最近は、鏡館に遊びに行っていないです。久しぶりに行ってみようかな？'
         else:
             pairs = []
             for p in posts:
@@ -25,10 +44,7 @@ class Quote(webapp2.RequestHandler):
             line = pairs[lr][0]
             link = pairs[lr][1]
 
-            msg = self.shorten_msg(line) + ' ' + link
-
-        account = Account()
-        account.tweet(msg)
+            return self.shorten_msg(line) + ' ' + link
 
     def shorten_msg(self, line):
         len_limit = 140 - 20 - 1  # XXX remove magic number
